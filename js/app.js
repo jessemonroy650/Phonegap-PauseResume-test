c63aa369b184a5d4cc330fde0c30593fea5d7d28
Jesse Monroy <jessemonroy650@yahoo.com>
2016-01-06 17:10:56 -0700

    update

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 //
 function onDeviceReady() {
     //alert("device ready.");
+    console.log("device ready.");
     if (device.platform === "iOS") {
         //alert("got iOS.");
         // hide Exit button. They don't have one on iOS devices.
@@ -12,9 +13,24 @@ function onDeviceReady() {
         /* deals with post-iOS-7 change that covers the status bar */
         /* http://coenraets.org/blog/2013/09/phonegap-and-cordova-with-ios-7/ */
         document.body.style.marginTop = "20px";
+    } else if (device.platform == 'Android') {
+        // Get rid of 300ms delay 
+        document.addEventListener('DOMContentLoaded', function() { FastClick.attach(document.body); }, false);
+        //
+        document.getElementById('exitApp').addEventListener('click', function() {
+           navigator.app.exitApp();
+        });
+        document.getElementById('theButton').addEventListener('click', function() {
+           onButtonClick();
+        });
+    } else if (device.platform == 'browser') {
+        document.getElementById('exitApp').addEventListener('click', function() {
+           alert('Exit button clicked.');
+        });
+        document.getElementById('theButton').addEventListener('click', function() {
+           onButtonClick();
+        });
     }
-    // Get rid of 300ms delay 
-    document.addEventListener('DOMContentLoaded', function() { FastClick.attach(document.body); }, false);
     //
     document.addEventListener("pause", onPause, false);
     document.addEventListener("resume", onResume, false);
@@ -22,31 +38,28 @@ function onDeviceReady() {
 
 
 function onButtonClick() {
-    //alert("button clicked.");
-    var perAfterResumeValue = persistentStorage.getItem("theValue");
-    document.getElementById('permInfo').innerHTML = perAfterResumeValue;
+    alert("button clicked.");
+    document.getElementById('content').innerHTML = window.localStorage.getItem("theValue");
 }
 
 var onPause = function() {
-    //document.getElementById('content').innerHTML = 'pause';
     var theValue = document.getElementById('theValue').value;
     document.getElementById('content').innerHTML = theValue;
 
     // Put Data to localstorage
-    persistentStorage.setItem("theValue", theValue);
-    sessionStorage.setItem("theValue", theValue);
+    window.localStorage.setItem("theValue", theValue);
+    window.sessionStorage.setItem("theValue", theValue);
 };
 
 var onResume = function() {
     alert('got onResume');
     // Get Data to localstorage
-    var perAfterResumeValue = persistentStorage.getItem("theValue");
-    var sesAfterResumeValue = sessionStorage.getItem("theValue");
-
-    document.getElementById('info').innerHTML = perAfterResumeValue;
+    document.getElementById('peristentInfo').innerHTML = window.localStorage.getItem("theValue");
+    document.getElementById('sessionInfo').innerHTML   = window.sessionStorage.getItem("theValue");
 };
 
-var persistentStorage = window.localStorage;
-var sessionStorage = window.sessionStorage;
+var device = {platform:"browser"};
 
 document.addEventListener("deviceready", onDeviceReady, false);
+
+document.addEventListener("DOMContentLoaded", onDeviceReady, false);
